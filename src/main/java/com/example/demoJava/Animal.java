7210285ebd2328369361cdee2f9dae3847ebd5f6
Jairo <jairoalonsoarroyave@gmail.com>
2020-08-15 22:19:21 -0500

    develop  commit ---2

--- src/main/java/com/example/demoJava/Animal.java
+++ src/main/java/com/example/demoJava/Animal.java
@@ -1,14 +1,9 @@
 public class Animal {
 
-<<<<<<< HEAD
-    int eb = 12;
-=======
+    int b = 19;
     int a = 19;
->>>>>>> develop  commit ---
     int s = 19;
     int c = 19;
     int d = 19;
-    int vv = 12;
-    int z = 12;
-
+    int eb = 12;
 }
